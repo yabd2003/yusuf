5598fb199adaa7be1049ad576a4a3b2e052fc229
ABDIQADIR YUSUF A <yabd2003@education.mpls.k12.mn.us>
2017-09-08 09:56:20 -0500

    added python files

---
+++ def function( argument).py
@@ -0,0 +1,10 @@
+def hello():
+    print "Hello world"
+    
+def hellovar():
+    a = "Hi"
+    print a
+
+def helloinput():
+        name = raw_input("What is your name? ")
+        print "Hello " + name
